7f94e6152c9a214ee015e86c017aabc7091671a4
megha <meghamini40@gmail.com>
2022-11-10 14:21:09 +0530

    increase skills

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -6,31 +6,31 @@ const Skills = () => {
       <h2 className="text-4xl mt-4 my-8">Skills</h2>
       <div className="grid sm:grid-cols-1 md:grid-cols-2 sm:mx-0 md:mx-5 text-start">
         <div className="sm:mx-0 md:mx-16">
-          <span>React</span>
+          <span>React App</span>
           <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-300 my-2">
-            <div class="bg-blue-600 h-2.5 rounded-full w-64"></div>
+            <div class="bg-blue-600 h-2.5 rounded-full w-80"></div>
           </div>
           <span>React Native</span>
           <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-300 my-2">
-            <div class="bg-blue-600 h-2.5 rounded-full w-52"></div>
+            <div class="bg-blue-600 h-2.5 rounded-full w-72"></div>
           </div>
           <span>Redux</span>
           <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-300 my-2">
-            <div class="bg-blue-600 h-2.5 rounded-full w-40"></div>
+            <div class="bg-blue-600 h-2.5 rounded-full w-64"></div>
           </div>
         </div>
         <div className="sm:mx-0 md:mx-16">
           <span>Javascript</span>
           <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-300 my-2">
-            <div class="bg-blue-600 h-2.5 rounded-full w-56"></div>
+            <div class="bg-blue-600 h-2.5 rounded-full w-72"></div>
           </div>
           <span>HTML5</span>
           <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-300 my-2">
-            <div class="bg-blue-600 h-2.5 rounded-full w-64"></div>
+            <div class="bg-blue-600 h-2.5 rounded-full w-80"></div>
           </div>
           <span>CSS</span>
           <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-300 my-2">
-            <div class="bg-blue-600 h-2.5 rounded-full w-56"></div>
+            <div class="bg-blue-600 h-2.5 rounded-full w-80"></div>
           </div>
         </div>
       </div>
