7f3944681b3b79c21845ab6440d6595bc55e2074
megha <meghamini40@gmail.com>
2022-11-10 13:41:59 +0530

    added hash route

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -8,16 +8,16 @@ import {GrLinkedin} from 'react-icons/gr';
 import {FaTwitter} from 'react-icons/fa';
 const Home = () => {
   return (
-    <section >
+    <section id='home'>
       <div className='flex flex-col items-center justify-center h-80 text-center
       bg-gradient-to-bl
       from-green-400 to-red-400 via-purple-300 animate-gradient-x text-white'>
           <h1 className=' text-5xl h-16'>Megha Maurya</h1>
           <span className='text-xl'>A Frontend Developer</span>
           <div className='flex text-4xl -mb-20 my-4'>
-            <a href='https://github.com/meghamaurya' target='_blank' rel="noopener noreferrer"><BsGithub className='m-2'/></a>
-            <a href='https://www.linkedin.com/in/megha-maurya-21369324b/' target='_blank' rel="noopener noreferrer"><GrLinkedin className='m-2'/></a>
-            <a href='https://twitter.com/MeghaMaurya58' target='_blank' rel="noopener noreferrer"><FaTwitter className='m-2'/></a>
+            <a href='https://github.com/meghamaurya' target='_blank' rel="noopener noreferrer"><BsGithub className='m-2 hover:text-neutral-600'/></a>
+            <a href='https://www.linkedin.com/in/megha-maurya-21369324b/' target='_blank' rel="noopener noreferrer"><GrLinkedin className='m-2 hover:text-neutral-600'/></a>
+            <a href='https://twitter.com/MeghaMaurya58' target='_blank' rel="noopener noreferrer"><FaTwitter className='m-2 hover:text-neutral-600'/></a>
           </div>
       </div>
       <Aboutme />
