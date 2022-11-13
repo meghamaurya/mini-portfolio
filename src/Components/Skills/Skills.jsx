c1b7b85685e581e588163de8465f6060d5aff248
megha <meghamini40@gmail.com>
2022-11-14 01:48:09 +0530

    fixed skills responsiveness

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -8,29 +8,29 @@ const Skills = () => {
         <div className="sm:mx-0 md:mx-16">
           <span>React App</span>
           <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-300 my-2">
-            <div class="bg-blue-600 h-2.5 rounded-full w-80"></div>
+            <div class="bg-blue-600 h-2.5 rounded-full w-10/12"></div>
           </div>
           <span>React Native</span>
           <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-300 my-2">
-            <div class="bg-blue-600 h-2.5 rounded-full w-72"></div>
+            <div class="bg-blue-600 h-2.5 rounded-full w-9/12"></div>
           </div>
           <span>Redux</span>
           <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-300 my-2">
-            <div class="bg-blue-600 h-2.5 rounded-full w-64"></div>
+            <div class="bg-blue-600 h-2.5 rounded-full w-8/12"></div>
           </div>
         </div>
         <div className="sm:mx-0 md:mx-16">
           <span>Javascript</span>
           <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-300 my-2">
-            <div class="bg-blue-600 h-2.5 rounded-full w-72"></div>
+            <div class="bg-blue-600 h-2.5 rounded-full w-9/12"></div>
           </div>
           <span>HTML5</span>
           <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-300 my-2">
-            <div class="bg-blue-600 h-2.5 rounded-full w-80"></div>
+            <div class="bg-blue-600 h-2.5 rounded-full w-10/12"></div>
           </div>
           <span>CSS</span>
           <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-300 my-2">
-            <div class="bg-blue-600 h-2.5 rounded-full w-80"></div>
+            <div class="bg-blue-600 h-2.5 rounded-full w-10/12"></div>
           </div>
         </div>
       </div>
