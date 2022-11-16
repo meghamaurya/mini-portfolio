2613009cf844f1913dc0967c2da7764cbaa4773a
megha <meghamini40@gmail.com>
2022-11-16 15:18:59 +0530

    update resume

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import img from './image/cv1.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import img from "./image/resume.png";
 const Resume = () => {
   return (
-    <div className='m-auto py-12 text-center'>
-      <Link to="/" className=' animate-pulse text-blue-600 font-semibold'>Back</Link>
-      <img src={img} alt='resume' width="600px" className='px-5 mx-auto border-4 border-blue-500'/>
+    <div className="m-auto py-16 text-center">
+      <Link to="/" className=" animate-pulse text-blue-600 font-semibold">
+        Back
+      </Link>
+      <img
+        src={img}
+        alt="resume"
+        width="600px"
+        className="px-5 mx-auto border-4 border-blue-500"
+      />
     </div>
-  )
-}
+  );
+};
 
 export default Resume;
-
